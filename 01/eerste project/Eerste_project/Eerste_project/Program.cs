a0bbff13b6f73ac253e00cdecb7f20bb24829741
plankmannetje03 <38730@ma-web.nl>
2025-06-24 14:22:24 +0200

    alle files toegevoegd

---
+++ 01/eerste project/Eerste_project/Eerste_project/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Eerste_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int nummer = 10;
+            string videogameCharacter = "Link";
+            bool isActief = false;
+            float getal = 7.5f;
+
+            Console.WriteLine("Int: " + nummer);
+            Console.WriteLine("String: " + videogameCharacter);
+            Console.WriteLine("Bool: " + isActief);
+            Console.WriteLine("Float: " + getal);
+
+           
+            Console.ReadLine();
+        }
+    }
+}
+
